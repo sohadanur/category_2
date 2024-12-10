aac3ee3da84b37445db9cd4fbd28308f0205c5b2
Sohada Hasan <sohadanur01@gmail.com>
2024-12-10 20:30:29 +0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,5 +3,9 @@ from .views import CategoryView
 
 urlpatterns = [
     path('categories/', CategoryView.as_view(), name='category-list'),  # For GET all and POST
-    path('categories/<int:pk>/', CategoryView.as_view(), name='category-detail'),  # For GET, PUT, DELETE
+    #path('categories/<int:pk>/', CategoryView.as_view(), name='category-detail'),  # For GET, PUT, DELETE
+    path('categories/', CategoryView.as_view(), name='category-list'),  # For GET all
+    path('categories/create/', CategoryView.as_view(), name='category-create'),  # For POST
+    path('categories/<int:pk>/', CategoryView.as_view(), name='category-detail'),  # For GET by ID
+    path('categories/<int:pk>/edit/', CategoryView.as_view(), name='category-update-delete'),  # For PUT/DELETE
 ]
